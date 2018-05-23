3bcb2e5c9f1bc044936d37d62995193565ba8ff1
Rodrigues, Ana Laura <ana.laura.rodrigues@accenture.com>
2018-05-23 01:23:53 -0300

    ATM - Cash Machine Application

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# ATM_Machine
-Cash machine simulator.
+# Project: ATM - Cash Machine
+
+## Project overview
+
+Simulator - delivery of notes when a client does a withdraw in a cash machine.
+
+### Installing
+
+Clone the repository and follow the steps below:
+
+```js
+$ git clone
+$ npm install
+$ npm install --global gulp-cli
+$ npm run dev  
+```
+
+## Built With
+
+* [Bootstrap](https://getbootstrap.com/) - Open source toolkit (v3.3.7)
+* [jQuery](https://jquery.com/) - Dependency Management
+* [Sass](https://sass-lang.com/) - Syntactically Awesome StyleSheets
+
+## Author
+
+* **Ana Laura Arezo** - [E-mail](mailto:ana.laura.rodrigues@accenture.com) - [LinkedIn](https://www.linkedin.com/in/anaarezo/?locale=en_US)
---
+++ dev/js/main.js
@@ -0,0 +1,83 @@
+var validateValue = {
+    init: function(){
+        this.validateValue();
+    },
+    validateValue: function() {
+        $('#txtInputWithdraw').on('change keyup focus', function() {
+            var removeLetters = $(this).val().replace(/[^0-9]/g, '');
+            $(this).val(removeLetters);
+        });
+    }
+}
+/*
+var withDraw = {
+    init: function() {
+        this.withDraw();
+    },
+    withDraw:function() {
+        console.log('-------- Extract --------');
+        
+        $('#btnGetMoney').click(function() {
+
+            var money = $('#txtInputWithdraw').val();
+            // Checks if the field is empty
+            if($('#txtInputWithdraw').val() == ''){
+                $('.atm-footer p').addClass('warning');
+                console.log('------ Empty Field ------');
+            }else{
+                $('.atm-footer p').removeClass('warning');
+                console.log('------- Value here -------');
+                console.log('Withdraw value: $ '+ money +',00');
+            }
+        });
+    }
+};*/
+
+var verifyNotes = {
+    init: function() {
+        this.verifyNotes();
+    },
+    verifyNotes: function(){
+        $('#btnGetMoney').click(function(){
+            //var monay = $('#txtInputWithdraw').val();
+            //var value = 490;
+            var monay = $('#txtInputWithdraw').val();
+            var notas100 = 0;
+            var notas50 = 0;
+            var notas20 = 0;
+            var notas10 = 0;
+            while (monay > 0) {
+                if (monay > 100) {
+                    notas100 = Math.floor(monay/100);
+                    monay = monay % 100;
+                    continue;
+                }
+                else if (monay > 50) {
+                    notas50 = Math.floor(monay/50);
+                    monay = monay % 50;
+                    continue;
+                }
+                else if (monay > 20) {
+                    notas20 = Math.floor(monay/20);
+                    monay = monay % 20;
+                    continue;
+                }
+                else if (monay > 10) {
+                    notas10 = Math.floor(monay/10);
+                    monay = monay % 10;
+                    continue;
+                }		
+            }
+            console.log('notas100:'+notas100);
+            console.log('notas50:'+notas50);
+            console.log('notas20:'+notas20);
+            console.log('notas10:'+notas10);
+        });
+    }
+}
+
+$(document).ready(function () {
+    validateValue.init();
+    //withDraw.init();
+    verifyNotes.init();
+});
